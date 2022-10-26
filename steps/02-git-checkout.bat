23ec762f6de31c8dc270ba0dbb40bf60daa23c33
Alberto Maghini (MSC Technology Italia) <a.maghini@gmail.com>
2022-10-26 10:34:51 +0200

    restored previous del command

--- steps/02-git-checkout.bat
+++ steps/02-git-checkout.bat
@@ -2,10 +2,9 @@ set branch=%1
 set has_branch=
 git show-ref refs/heads/%branch% > .tmp
 set /p has_branch=<.tmp
+del /q .tmp
 if "%has_branch%" == "" (
     git checkout -b %branch%
-    del /q .tmp
     goto :eof
 )
 git checkout %branch%
-del /q .tmp
