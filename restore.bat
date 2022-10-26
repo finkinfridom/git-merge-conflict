cb3176fdcafd973b7391e0621d7538ba41fd04d1
Alberto Maghini (MSC Technology Italia) <a.maghini@gmail.com>
2022-10-26 10:40:07 +0200

    reviewed restore.bat

--- restore.bat
+++ restore.bat
@@ -1,4 +1,7 @@
-set main_branch_name=%1
+set new_branch_name=%1
+if not defined new_branch_name set new_branch_name=feat/update-readme
+set main_branch_name=%2
 if not defined main_branch_name set main_branch_name=main
-
+ 
 git checkout %main_branch_name%
+git branch -D %new_branch_name%
