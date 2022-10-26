f82a146083ec9578da56efe610c5a64e0e1738eb
Alberto Maghini (MSC Technology Italia) <a.maghini@gmail.com>
2022-10-26 10:00:00 +0200

    removed additional commit and push

--- steps/03-touch-file-on-branch.bat
+++ steps/03-touch-file-on-branch.bat
@@ -3,6 +3,3 @@ if not exist ./conflicting-file.md goto :eof
 echo # This is going to be the content on %1 > ./conflicting-file.md
 
 git add conflicting-file.md
-git commit -m "Update file"
-
-git push origin %1
