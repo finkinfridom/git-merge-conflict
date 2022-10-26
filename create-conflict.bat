c359bd18069a79d052354e35d67d7eb2f18d751a
Alberto Maghini <a.maghini@gmail.com>
2022-10-26 14:02:50 +0200

    reviewed entrypoints

--- create-conflict.bat
+++ create-conflict.bat
@@ -4,27 +4,27 @@ if not defined new_branch_name set new_branch_name=feat/update-file
 set main_branch_name=%2
 if not defined main_branch_name set main_branch_name=main
 
-echo "perform 'git pull origin %main_branch_name%'"
+echo "####### perform 'git pull origin %main_branch_name%' #######"
 call ./steps/00-git-pull-main.bat %main_branch_name%
 pause
-echo "creating file on '%main_branch_name%'"
+echo "####### creating file on '%main_branch_name%' #######"
 call ./steps/01-create-file-on-main.bat %main_branch_name%
 pause
-echo "perform 'git checkout -b %new_branch_name%'"
+echo "####### perform 'git checkout -b %new_branch_name%' #######"
 call ./steps/02-create-new-branch.bat %new_branch_name%
 pause
-echo "updating file on '%main_branch_name%'"
+echo "####### updating file on '%main_branch_name%' #######"
 call ./steps/03-update-file-on-main.bat %main_branch_name%
 pause
-echo "perform 'git checkout %new_branch_name%"
+echo "####### perform 'git checkout %new_branch_name% #######"
 call ./steps/04-git-checkout-new-branch.bat %new_branch_name%
 pause
-echo "updating file on %new_branch_name%"
+echo "####### updating file on %new_branch_name% #######"
 call ./steps/05-touch-file-on-branch.bat %new_branch_name%
 pause
-echo "committing file on '%new_branch_name%'"
+echo "####### committing file on '%new_branch_name%' #######"
 call ./steps/06-commit-and-push-on-new-branch.bat %new_branch_name%
 pause
-echo "perform 'git merge origin %main_branch_name%'"
+echo "####### perform 'git merge origin %main_branch_name%' #######"
 call ./steps/07-git-merge-main.bat %main_branch_name%
 pause
--- restore.bat
+++ restore.bat
@@ -2,10 +2,21 @@ set new_branch_name=%1
 if not defined new_branch_name set new_branch_name=feat/update-file
 set main_branch_name=%2
 if not defined main_branch_name set main_branch_name=main
- 
+
 git checkout %main_branch_name%
-git branch -D %new_branch_name%
+git pull
+
+git show-ref refs/heads/%new_branch_name% > .tmp
+set /p has_branch=<.tmp
+del /q .tmp
+if not "%has_branch%" == "" (
+    
+    git branch -D %new_branch_name%
+)
+
+pause
 
 git checkout %main_branch_name%
 git reset --hard HEAD~1
+pause
 git push --force
