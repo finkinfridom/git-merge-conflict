bc5c950d1b6931f0727996f84ce36aaf86b3aca7
Alberto Maghini <a.maghini@gmail.com>
2022-10-26 10:52:17 +0200

    fixed create branch script

--- steps/02-create-new-branch.bat
+++ steps/02-create-new-branch.bat
@@ -3,7 +3,7 @@ set has_branch=
 git show-ref refs/heads/%branch% > .tmp
 set /p has_branch=<.tmp
 del /q .tmp
-if "%has_branch%" == "" (
+if not "%has_branch%" == "" (
     git branch -D %branch%
 )
 git checkout -b %branch%
